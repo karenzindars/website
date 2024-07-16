76c6002f6a1212ea65d07bef09dbab0d8bb1988d
globalupright <doesthiswork@mail.com>
2024-07-16 12:26:44 -0700

    remove contact info, ahead of redoing ui

--- website/src/Components/Common/ContinuingEducationCourses.js
+++ website/src/Components/Common/ContinuingEducationCourses.js
@@ -3,27 +3,7 @@ import React from 'react'
 export default function ContinuingEducationCourses() {
   return (
     <div>
-      <h4>
-        <span style={{fontSize: '19px', lineHeight: '18px'}}>
-          <strong>&nbsp;{new Date().getFullYear()} Classes for Feldenkrais®&nbsp;</strong>
-        </span>
-        <strong>
-          Practitioners and Trainees
-        </strong>
-      </h4>
-      <p>
-        <a rel="noreferrer" target="_blank" id='external-img' href='https://user-images.githubusercontent.com/36345325/218910138-e5506911-7a60-4a75-a882-a3ebdee76e5f.png'>
-          <img className="fmethod-img"
-            alt="2022 Flyer" src="img/2023flyermymomisthebest.png" />
-        </a>
-      </p>
-      <h4 style={{fontWeight: 100}}>
-        To register: email Karen at{' '}
-        <a href="mailto:karenzindars@gmail.com"
-           style={{color: '#6a6a6a'}}>
-          karenzindars@gmail.com
-        </a>
-      </h4>
+      <h1>how about no!</h1>
     </div>
   )
 }
--- website/src/Components/Common/MentoringSchedule.js
+++ website/src/Components/Common/MentoringSchedule.js
@@ -3,39 +3,7 @@ import React from 'react'
 export default function MentoringSchedule() {
   return (
     <div>
-      <h3>
-        <span style={{ color: '#0000ff' }}>
-          Small Group Mentoring - 3 to 6 people
-        </span>
-      </h3>
-      <p>
-        $100/ person for 3 people - 2 hours
-      </p>
-      <p>
-        $75/person for 4 - 6 people - 2 hours
-      </p>
-      <p>
-        Location - 1979 17th Avenue San Francsico, CA 94116
-      </p>
-      <div>&nbsp;___________________________________________</div>
-      <h3>
-        <span style={{ color: '#0000ff' }}>
-          Private mentoring sessions - 1 to 2 people
-        </span>
-      </h3>
-      <p>
-        $150/hour
-      </p>
-      <p>
-        Location - 1979 17th Avenue San Francsico, CA 94116
-      </p>
-      <div>&nbsp;___________________________________________</div>
-      <p>
-        Please contact Karen directly at 415-518-7588 or email{' '}
-        <a href="mailto:karenzindars@gmail.com">
-          karenzindars@gmail.com
-        </a>
-      </p>
+      <h1>i got something you can mentor right here pal</h1>
     </div>
   )
 }
