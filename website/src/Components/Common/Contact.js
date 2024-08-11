23120a7cde0cb23b9701eb08b620d60ab2de9518
Evan Ugarte <evanuxd@gmail.com>
2024-08-11 01:14:04 -0700

    make submit button wider for mobile view

--- website/src/Components/Common/Contact.js
+++ website/src/Components/Common/Contact.js
@@ -85,7 +85,7 @@ export default function Contact() {
           <button
             disabled={loading}
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            className="text-white bg-blue-700 w-full md:w-1/4 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
               {submitText}
             </button>
         </form>
