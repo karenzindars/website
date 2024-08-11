6ec0438c2860b265ee49baaeced867c5548af485
Evan Ugarte <evanuxd@gmail.com>
2024-08-11 00:49:31 -0700

    log error on email failure, clear form on success

--- website/src/Components/Common/Contact.js
+++ website/src/Components/Common/Contact.js
@@ -40,14 +40,17 @@ export default function Contact() {
         },
       )
       .then(
-        (result) => {
+        () => {
           setSubmitText('Message sent!');
           setTimeout(() => {
+            // Clears the form after sending the email
+            e.target.reset();
             setLoading(false);
             setSubmitText(DEFAULT_BUTTON_TEXT);
           }, 5000); // hide message after 5 seconds
         },
         (error) => {
+          console.error(error)
           alert('an error occurred submitting the form try again in a few minutes or email my mom if you know her email!');
           setTimeout(() => {
             setLoading(false);
@@ -55,9 +58,6 @@ export default function Contact() {
           }, 5000); // hide message after 5 seconds
         }
       );
-
-    // Clears the form after sending the email
-    e.target.reset();
   };
   return (
     <section >
@@ -67,15 +67,15 @@ export default function Contact() {
         <form ref={form} onSubmit={sendEmail}>
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+            <input disabled={loading} type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
           </div>
           <div>
             <label htmlFor="subject" className="block my-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
+            <input disabled={loading} type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="message" className="block my-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+            <textarea disabled={loading} id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
           <div className='my-5'>
             <ReCAPTCHA ref={recaptcha} sitekey={RECAPTCHA_PUBLIC_KEY} />
